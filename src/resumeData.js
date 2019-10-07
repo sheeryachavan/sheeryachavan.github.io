454db107b89829024af908bda6acb371ae0108e1
sheeryachavan <shreesh.chavan@gmail.com>
2019-10-07 19:12:08 -0400

    initial commit

---
+++ src/resumeData.js
@@ -0,0 +1,106 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Shreesh Chavan",
+    "role": "Frontend Designer and Developer",
+    "linkedinId":"shreesh-chavan",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/shreesh-chavan/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/sheeryachavan",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"http://twitter.com/rbhatia46",
+          "className":"fa fa-facebook"
+        }
+      ],
+    "aboutme":"I am a graduate student from Stevens Institte of Technology, Hoboken, NJ, currently pursuing my masters in Computer Science. I am looking for a full time opportunity in Software Development.",
+    "address":"United States",
+    "website":"https://sheeryachavan.github.io",
+    "education":[
+      {
+        "UniversityName":"Stevens Institute of  Technology",
+        "specialization":"Computer Science",
+        "MonthOfPassing":"December",
+        "YearOfPassing":"2019",
+        "Achievements":"Graduate Assistant - Managing Stevens Tech Publisher Website"
+      },
+      {
+        "UniversityName":"St. John COllege of Engineering and Management",
+        "specialization":"Computer Engineering",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2017",
+        "Achievements":"Chief Editor - Spectrum College Magazine"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
